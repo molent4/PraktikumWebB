dd29d16d64820a97860d011d40ab07a7c2c0b5a7
molent4 <57343997+molent4@users.noreply.github.com>
2020-10-27 11:20:41 +0800

    fix margin section

--- Tugas_13/halaman_login.php
+++ Tugas_13/halaman_login.php
@@ -6,38 +6,42 @@
 	<link rel="stylesheet" type="text/css" href="CSS/halaman_login.css">
 </head>
 <body>
-	<?php 
-		include "koneksi.php";
-		if( isset($_SESSION['status'])) {
-			header("Location:index.php");
-			exit;
-		}
-		if (isset($_GET['pesan'])) {
-			if ($_GET['pesan']=="gagal_login") {
-				echo "<script type='text/javascript'>alert('Username dan Password Salah!');</script>";
+	<div class="container">
+		<?php 
+			include "koneksi.php";
+			if( isset($_SESSION['status'])) {
+				header("Location:index.php");
+				exit;
 			}
-		}
-	?>
-	<nav class="header">
-		<ul>
-			<li><a href="halaman_login.php">Login</a></li>
-			<li><a href="halaman_daftar.php">Daftar</a></li>
-		</ul>
-	</nav>
-	<h1>Selamat Datang Di Perpustakaan Bersama</h1>
-	<div id="login_box">
-		<h2>LOG IN</h2>
-		<form method="POST" action="login.php">
-			<label>Username</label><br>
-			<input type="text" name="username" placeholder="Username"><br>
-			<label>Password</label><br>
-			<input type="password" name="pass" placeholder="Password"><br>
-			<input type="submit" name="masuk" value="Login">
-		</form>
+			if (isset($_GET['pesan'])) {
+				if ($_GET['pesan']=="gagal_login") {
+					echo "<script type='text/javascript'>alert('Username dan Password Salah!');</script>";
+				}
+			}
+		?>
+		<nav class="header">
+			<ul>
+				<li><a href="halaman_login.php">Login</a></li>
+				<li><a href="halaman_daftar.php">Daftar</a></li>
+			</ul>
+		</nav>
+		<div class="content">
+		<h1>Selamat Datang Di Perpustakaan Bersama</h1>
+		<div id="login_box">
+			<h2>LOG IN</h2>
+			<form method="POST" action="login.php">
+				<label>Username</label><br>
+				<input type="text" name="username" placeholder="Username"><br>
+				<label>Password</label><br>
+				<input type="password" name="pass" placeholder="Password"><br>
+				<input type="submit" name="masuk" value="Login">
+			</form>
+		</div>
+		</div>
+		<footer>
+			<p>&copy Copyright 2020</p>  
+			<p>Perpustakaan Bersama, Bali</p>
+		</footer>
 	</div>
-	<footer>
-		<p>&copy Copyright 2020</p>  
-        <p>Perpustakaan Bersama, Bali</p>
-	</footer>
 </body>
 </html>
